db2bcd1c4ace3dd43adec2c6c81366b24532bd6f
kiriharu <kiriharu@github.com>
2018-11-05 01:44:36 +0300

    added sending to more than 1 chat

--- tgsender.py
+++ tgsender.py
@@ -6,9 +6,9 @@ import telebot
 from telebot import util
 from telebot import apihelper
 import argparse
-#apihelper.proxy = Прокси, если надо.
+#apihelper.proxy = прокси, если надо.
 token = 'Сюда токен от бота'
-chat_id = 'Сюда id чата, в который будем всё отсылать. Chat ID также можно задавать при помощи --chat_id'
+chat_id = ['первый id', 'второй id']
 bot = telebot.TeleBot(token)
 parser = argparse.ArgumentParser(
     prog='tgsender',
@@ -20,33 +20,37 @@ parser.add_argument('--parse_mode',
     default='markdown')
 parser.add_argument('--chat_id',
     help='Chat ID',
-    default=chat_id)
+    action='append')
 parser.add_argument('--file',
 	help='path to file',
 	default=None)
 
 args = parser.parse_args()
 
-#Если количество аргументов равно 1 (просто pinger.py) - показываем help и выходим.
+#если ничего не введено (стреляем из пушки по воробьям при помощи sys, ага) - выводим help
 if len(sys.argv) == 1:
 	parser.print_help()
 	exit()
 
-#Если в аргуметах есть --file открываем и отсылаем его как документ.
-if args.file:
-	data = open(args.file, 'rb')
-	bot.send_document(chat_id, data)
+#проверка введенных chatid, их кстати можно указывать несколько (--chat_id=1234 --chat_id=4321)
+if args.chat_id:
+	chat_id = args.chat_id
+	print(chat_id)
 
-#Если в аргументах есть --text...
+#Отправка файлов
+if args.file:
+	for chat in chat_id:
+		data = open(args.file, 'rb')
+		bot.send_document(chat, data)
+
+#Отправка текста
 if args.text:
-	try:
-		#Пытаемся его отправить обычным методом.
-		bot.send_message(args.chat_id,
-	    args.text,
-	    parse_mode=args.parse_mode)
-	except:
-		#Если сообщение слишком длинное
-		splitted_text = util.split_string(args.text, 3000)
-		for text in splitted_text:
+	for chat in chat_id:
+		try:
+			bot.send_message(chat, args.text, parse_mode=args.parse_mode)
+		except:
+			#Если сообщение слишком длинное
+			splitted_text = util.split_string(args.text, 3000)
+			for text in splitted_text:
 			#Не используем markdown, ибо это может вызвать ошибки :shrug
-			bot.send_message(chat_id, text)
+				bot.send_message(chat, text)
