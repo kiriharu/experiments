e668ddd6dbd9cd94b2b39057232ccaf34ad7dc62
kiriharu <38693046+kiriharu@users.noreply.github.com>
2020-06-25 11:42:56 +0300

    rewrited with option parsing

--- shellscripts/bash_telegram_notify.sh
+++ shellscripts/bash_telegram_notify.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
-TOKEN=$1
-RECIPIENTS_ID=(`echo $2 | sed 's/,/\n/g'`)
 API_URL="https://api.telegram.org"
-TEXT=`cat`
-if [[ -z $TOKEN ]] || [[ -z $2 ]] ; then
-  echo "Usage: ./bash_telegram_notify.sh botTELEGRAM_BOT_TOKEN userid1,userid2,userid3"
-  echo "Example: ./bash_telegram_notify.sh bot1232145125:AAAAAAAAA_BBBBBBB 13213123,23123213,213213"
+while getopts "t:u:" opt
+do
+case $opt in
+  t)TOKEN=$OPTARG
+    ;;
+  u)RECIPIENTS_ID=(`echo $OPTARG | sed 's/,/\n/g'`)
+    ;;
+esac
+done
+
+shift $((OPTIND - 1))
+TEXT=$1
+
+if [[ -z $TOKEN ]] || [[ -z $RECIPIENTS_ID ]] ; then
+  echo "Usage: ./bash_telegram_notify.sh -t botTELEGRAM_BOT_TOKEN -u userid1,userid2,userid3 message "
+  echo "Example: ./bash_telegram_notify.sh -t bot1232145125:AAAAAAAAA_BBBBBBB -u 13213123,23123213,213213 hellow!!!"
   exit 1
 fi
 
 function send_message(){
   curl -s -X POST \
-  -Ftext=`cat` -Fchat_id=$1 -Fparse_mode="Markdown"\
+  -Ftext="`cat`" -Fchat_id=$1 -Fparse_mode="Markdown"\
   "$API_URL/$TOKEN/sendMessage"
 }
 
@@ -20,3 +30,4 @@ for RECIPIENT in "${RECIPIENTS_ID[@]}"
 do
   echo $TEXT | send_message $RECIPIENT
 done
+
